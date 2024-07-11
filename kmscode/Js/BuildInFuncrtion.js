74a5872baaba2cb5e697169dc16b17fa6e83131a
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-11 23:00:21 +0700

    Init project

---
+++ kmscode/Js/BuildInFuncrtion.js
@@ -0,0 +1,8 @@
+let strTen='10';
+console.log(typeof(strTen));
+
+strTen--;
+console.log(strTen);
+console.log(strTen*10);
+console.log(Number.isInteger(1));
+console.log(Number.isFinite(-1));
---
+++ kmscode/Js/Operator.js
@@ -0,0 +1,27 @@
+let pen=10;
+let book=15;
+
+
+let pens= pen*3;
+let buy=pen+book;
+let expensive=book-pen;
+let deveded=book/pen;
+let pwoer=2**3;
+
+console.log(pens);
+console.log(buy);
+console.log(expensive);
+console.log(deveded);
+console.log(pwoer);
+
+
+//Math assigment operator
+let add=2;
+// add assignment operator
+ add +=4;
+
+ console.log(add);
+
+ let devide=10;
+ devide/=2;
+ console.log(devide);
