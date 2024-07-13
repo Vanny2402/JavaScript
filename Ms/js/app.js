44bf1f5f39a13a62e8d49fbafb346b6df2164e33
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-13 19:46:48 +0700

    End of Brank/Continu

--- Ms/js/app.js
+++ Ms/js/app.js
@@ -1,23 +1,8 @@
-const person={
-    id:1,
-    name:"VANNY",
-    sex: "male",
-    age:19,
-    education:true,
-    marrid:false,
-    siblings:["Makara","Minea","Mesa"],
-    greeting:function(){
-        console.log("Hello VANNY");
-    },
-
-};
-
-// for (let i in person){
-//     console.log(" ID: ",person[i], "\n Name: ",person[i],"\n Gender: ",person[i],"\n Age: ",person[i],
-//                 "\n Education: ",person[i],"\n Marrid: ",person[i],"\n Sibling: ",person[i],);
-//                 // "\n Greeting: ",person[i]);   
-// }   
-
-for (let i in person){
-    console.log(i, person[i]);
+for (let i=0;i<=10;i++){
+    if (i==5){
+        // break;
+        continue;
+    }
+    console.log("hey",i);
+    
 }
