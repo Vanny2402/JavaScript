0264ab129f174ebcd8befd8489f95ba5e669a493
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-13 19:14:09 +0700

    Object and array

---
+++ Ms/js/app.js
@@ -0,0 +1,29 @@
+
+const freinds=[
+    "TOLA",
+    "Male",
+    28,
+    false,
+    true,
+    ["Makara","Minea","Mesa"],
+    function greeting(){
+        console.log("Hello Tola");
+    },
+];
+
+// console.log(freinds[5][0],freinds[6]())
+
+const person={
+    id:1,
+    name:"VANNYss",
+    sex: "male",
+    age:19,
+    education:true,
+    marrid:false,
+    siblings:["Makara","Minea","Mesa"],
+    greeting:function(){
+        console.log("Hello VANNY");
+    },
+};
+
+console.log(person.name,person.greeting());
