bff517e742e783a2534355b8b07edb2189300f09
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-14 18:35:47 +0700

    end of Foreach

--- Ms/js/app.js
+++ Ms/js/app.js
@@ -1,38 +1,33 @@
+const name=["AB","BC","DE"];
 
-// let name =["A","B","C","D"];
-// console.log("To String: "+name.toString());
-// console.log("Join : ",name.join("*"));
-let name =["A","B","C","D"];
-// name.push("E");
-// // name.pop();
-// console.log(""+name.toString());
-// console.log(name);
-// name.unshift("Vanny");
-// console.log(name);
-// name.shift();
-// console.log(name);
+const people=[
+    {name:"Tola",sex:"male",possition:"Developer"},
+    {name:"Seyha",sex:"Female",possition:"QA"},
+    {name:"Banz",sex:"male",possition:"Cleaner"}
 
-// console.log(name);  
-// // name.splice(1,1,"VANNY","HONGKONG");// will remove B as it in index 1
-// name.splice(0,3); // Will remove abc
-// console.log(name);  
+];
+
+function showName(name){
+    console.log(name);
+    console.log("HMM");
+    
+}
+// name.forEach(showName);
 
 
-let boy=["Hong","HENG","LIN","NITA"];
-let number=[10,11,65,23,12,2,23,12,434,122,12,34,121,34,12,212];
-// const ful=boy.concat(name);
-// console.log(ful);
-// console.log(ful.join(""));
 
-// const sliceArr=boy.slice(1,3);
-// console.log(sliceArr);
-// console.log(sliceArr.join(" "));
+people.forEach(function(people){
+    console.log(people);
+})
 
-// console.log(name.reverse());
-// console.log(name.sort().join(" ,"));
 
-console.log(number.sort(
-    function(a,b){
-        return a-b;
-    }
-));
+//Long Form : 
+
+function logPerson(person) {
+    console.log(person);
+}
+
+for (let i = 0; i < people.length; i++) {
+    let currentPerson = people[i]; // Get the current person from the array
+    logPerson(currentPerson); // Call the function to log the person
+}
