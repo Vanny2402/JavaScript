2106f350e14d9d2ce48920128d86244d1a0391e1
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-14 23:34:17 +0700

    End of Map

--- Ms/js/app.js
+++ Ms/js/app.js
@@ -1,33 +1,36 @@
-const name=["AB","BC","DE"];
+const name = ["VANNY", "HONGKONG", "VIETNAME"];
+const number = [1, 2, 3, 4, 54, 5, 6];
 
-const people=[
-    {name:"Tola",sex:"male",possition:"Developer"},
-    {name:"Seyha",sex:"Female",possition:"QA"},
-    {name:"Banz",sex:"male",possition:"Cleaner"}
+const people = [{ name: "Tola", gender: "Male", Jobtile: "Cleaner" },
+{ name: "SONG", gender: "Female", Jobtile: "supprt" },
+{ name: "LEAP", gender: "Male", Jobtile: "Delivery" },
+]
 
-];
+const newName=name.map(showName);
 
 function showName(name){
+
     console.log(name);
-    console.log("HMM");
-    
 }
-// name.forEach(showName);
+console.log(showName);
+
+// name.map(function(names){
+//     console.log(names);
+// });
 
 
+// const n=number.map(function(num){
+//     return num*2;
+// });
 
-people.forEach(function(people){
-    console.log(people);
-})
+// console.log(n);
 
+const newPeople = people.map(function (person) {
+    return {
+        name: person.name + "sok",
+        gender: person.gender.toUpperCase(),
+        Jobtile: person.Jobtile.toUpperCase()
+    };
+});
 
-//Long Form : 
-
-function logPerson(person) {
-    console.log(person);
-}
-
-for (let i = 0; i < people.length; i++) {
-    let currentPerson = people[i]; // Get the current person from the array
-    logPerson(currentPerson); // Call the function to log the person
-}
+console.log(newPeople);
