470184e1f41e5058e7a131786a34f15339aa58af
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-07-15 08:11:02 +0700

    End of Filter

--- Ms/js/app.js
+++ Ms/js/app.js
@@ -1,36 +1,33 @@
-const name = ["VANNY", "HONGKONG", "VIETNAME"];
-const number = [1, 2, 3, 4, 54, 5, 6];
+// const name = ["VANNY", "HONGKONG", "VIETNAME"];
+// const number = [1, 2, 3, 4, 54, 5, 6];
 
 const people = [{ name: "Tola", gender: "Male", Jobtile: "Cleaner" },
-{ name: "SONG", gender: "Female", Jobtile: "supprt" },
+{ name: "SONG", gender: "Female", Jobtile: "supprtIT" },
 { name: "LEAP", gender: "Male", Jobtile: "Delivery" },
-]
+{ name: "Long", gender: "Femal", Jobtile: "PD" }
+];
 
-const newName=name.map(showName);
+// const male=people.filter(function(name){
+//     return name.gender =="Male";
+// })
 
-function showName(name){
+// console.log(male);
 
-    console.log(name);
+
+
+
+function showMale(person){
+    return person.gender=="Male";
 }
-console.log(showName);
+const male=people.filter(showMale);
+console.log(male);
 
-// name.map(function(names){
-//     console.log(names);
-// });
+// const person={id:1,name:"VANNY",sex: "male",age:19,education:true,marrid:false,siblings:["Makara","Minea","Mesa"],
+//     greeting:function(){
+//         console.log("Hello VANNY");
+//     }
+// };
 
-
-// const n=number.map(function(num){
-//     return num*2;
-// });
-
-// console.log(n);
-
-const newPeople = people.map(function (person) {
-    return {
-        name: person.name + "sok",
-        gender: person.gender.toUpperCase(),
-        Jobtile: person.Jobtile.toUpperCase()
-    };
-});
-
-console.log(newPeople);
+// for (let i in person){
+//     console.log(i, person[i]);
+// }
